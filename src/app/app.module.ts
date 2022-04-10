a8a17d329c280a69c9a01988b4c99717d49f4014
denis23x <damage.23x@gmail.com>
2022-04-10 12:59:12 +0200

    added throttle too many requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@
 
 import { Module, Global } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './auth/auth.module';
 import { CategoryModule } from './category/category.module';
 import { CoreModule } from './core';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Global()
 @Module({
@@ -15,11 +17,21 @@ import { UserModule } from './user/user.module';
       isGlobal: true,
       envFilePath: '.env'
     }),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 20
+    }),
     AuthModule,
     CategoryModule,
     CoreModule,
     PostModule,
     UserModule
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
   ],
   exports: [AuthModule, CategoryModule, CoreModule, PostModule, UserModule]
 })
