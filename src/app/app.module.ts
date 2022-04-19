fd6c4d9ad8ef73aa82012c9e865d75979d29157f
denis23x <damage.23x@gmail.com>
2022-04-19 19:44:18 +0200

    added cache module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { Module, Global } from '@nestjs/common';
+import { Module, Global, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './auth/auth.module';
@@ -8,7 +8,7 @@ import { CategoryModule } from './category/category.module';
 import { CoreModule } from './core';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 
 @Global()
 @Module({
@@ -16,6 +16,10 @@ import { APP_GUARD } from '@nestjs/core';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env'
+    }),
+    CacheModule.register({
+      ttl: 5,
+      max: 20
     }),
     ThrottlerModule.forRoot({
       ttl: 60,
@@ -28,6 +32,10 @@ import { APP_GUARD } from '@nestjs/core';
     UserModule
   ],
   providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor
+    },
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard
